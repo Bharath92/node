281b97772d8eba92a4cf45e476303ac32db89dc7
Bharath Upadhya <1992bharath@gmail.com>
2017-08-22 13:29:32 +0530

    arm

---
+++ scripts/Ubuntu_16.04_arm_Docker_1.12.6.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+readonly DOCKER_VERSION="1.12.6"
+
+# Indicates if docker service should be restarted
+export docker_restart=false
+
+setup_shippable_user() {
+  if id -u 'shippable' >/dev/null 2>&1; then
+    echo "User shippable already exists"
+  else
+    exec_cmd "sudo useradd -d /home/shippable -m -s /bin/bash -p shippablepwd shippable"
+  fi
+
+  exec_cmd "sudo echo 'shippable ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers"
+  exec_cmd "sudo chown -R $USER:$USER /home/shippable/"
+  exec_cmd "sudo chown -R shippable:shippable /home/shippable/"
+}
+
+check_docker_opts() {
+  # SHIPPABLE docker options required for every node
+  echo "Checking docker options"
+
+  SHIPPABLE_DOCKER_OPTS='DOCKER_OPTS="$DOCKER_OPTS -H unix:///var/run/docker.sock -g=/data --dns 8.8.8.8 --dns 8.8.4.4"'
+  opts_exist=$(sudo sh -c "grep '$SHIPPABLE_DOCKER_OPTS' /etc/default/docker || echo ''")
+
+  # DOCKER_OPTS do not exist or match.
+  if [ -z "$opts_exist" ]; then
+    echo "Removing existing DOCKER_OPTS in /etc/default/docker, if any"
+    sudo sed -i '/^DOCKER_OPTS/d' "/etc/default/docker"
+
+    echo "Appending DOCKER_OPTS to /etc/default/docker"
+    sudo sh -c "echo '$SHIPPABLE_DOCKER_OPTS' >> /etc/default/docker"
+    docker_restart=true
+  else
+    echo "Shippable docker options already present in /etc/default/docker"
+  fi
+
+  ## remove the docker option to listen on all ports
+  echo "Disabling docker tcp listener"
+  sudo sh -c "sed -e s/\"-H tcp:\/\/0.0.0.0:4243\"//g -i /etc/default/docker"
+}
+
+restart_docker_service() {
+  echo "checking if docker restart is necessary"
+  if [ $docker_restart == true ]; then
+    echo "restarting docker service on reset"
+    exec_cmd "sudo service docker restart"
+  else
+    echo "docker_restart set to false, not restarting docker daemon"
+  fi
+}
+
+install_ntp() {
+  {
+    check_ntp=$(sudo service --status-all 2>&1 | grep ntp)
+  } || {
+    true
+  }
+
+  if [ ! -z "$check_ntp" ]; then
+    echo "NTP already installed, skipping."
+  else
+    echo "Installing NTP"
+    exec_cmd "sudo apt-get install -y ntp"
+    exec_cmd "sudo service ntp restart"
+  fi
+}
+
+before_exit() {
+  echo $1
+  echo $2
+
+  echo "Node init script completed"
+}
+
+main() {
+  trap before_exit EXIT
+  exec_grp "setup_shippable_user"
+
+  trap before_exit EXIT
+  exec_grp "check_docker_opts"
+
+  trap before_exit EXIT
+  exec_grp "restart_docker_service"
+
+  trap before_exit EXIT
+  exec_grp "install_ntp"
+}
+
+main
